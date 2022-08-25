2e64dd2e6965742bd7eabf020f27faccf3a85532
Sylvain <sylvainterwilly@gmail.com>
2022-08-25 14:28:00 +0100

    📦 NEW: first commit project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { GameComponent } from './components/game/game.component';
+import { HomeComponent } from './components/home/home.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent },
+  {path:'home', component:HomeComponent},
+  {path:'game', component:GameComponent},
+  {path:'eshop', component:EshopComponent},
+  {path:'compte', component:MonCompteComponent},
+  {path:'cart', component:CartComponent},
+  {path:'**', component:NotFoundPageComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import * as fr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BarNavComponent } from './components/bar-nav/bar-nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
+import { EshopComponent } from './components/eshop/eshop.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { CartComponent } from './components/cart/cart.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarNavComponent,
+    FooterComponent,
+    HomeComponent,
+    GameComponent,
+    EshopComponent,
+    MonCompteComponent,
+    NotFoundPageComponent,
+    CartComponent,
+    AsideComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr-FR'
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    registerLocaleData(fr.default);
+  }
+}
